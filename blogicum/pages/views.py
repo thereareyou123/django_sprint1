aaf3bee4ff2d207100f470fda53ab7e15763b838
Никита Рохлин <thereareyou@yahoo.com>
2024-03-27 16:02:22 +0300

    now exactly ready

--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def rules(request):
+    return render(request, 'pages/rules.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
